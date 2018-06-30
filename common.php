21da776e8b4d6a742f88551c5b2e93064149f43a
dnqxj <dnqxj@icoud.com>
2018-06-30 18:12:18 +0800

    dnqxj
    添加空行

--- common.php
+++ common.php
@@ -1,4 +1,5 @@
 <?php
+
 //很好用的html压缩函数
 function compress_html($string) {
       return ltrim(rtrim(preg_replace(array("/> *([^ ]*) *</","/<!--[^!]*-->/","'/\*[^*]*\*/'","/\r\n/","/\n/","/\t/",'/>[ ]+</'),array(">\\1<",'','','','','','><'),$string)));
