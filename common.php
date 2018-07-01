dc45b128e68b0be8a63fa96c3deb32a577992878
dnqxj <39727296+dnqxj@users.noreply.github.com>
2018-07-01 16:08:17 +0800

    Update common.php
    新增dump,halt函数

--- common.php
+++ common.php
@@ -4,3 +4,22 @@
 function compress_html($string) {
       return ltrim(rtrim(preg_replace(array("/> *([^ ]*) *</","/<!--[^!]*-->/","'/\*[^*]*\*/'","/\r\n/","/\n/","/\t/",'/>[ ]+</'),array(">\\1<",'','','','','','><'),$string)));
 }
+
+//格式化输出函数
+if(file_exists('dump')){
+	function dump($data){
+		echo "<pre>";
+		print_r($data);
+		echo '</pre>';
+	}
+}
+
+//格式化输出函数,并中断
+if(file_exists('halt')){
+	function halt($data){
+		echo "<pre>";
+		print_r($data);
+		echo '</pre>';
+		exit;
+	}
+}
